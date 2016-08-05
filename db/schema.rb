9edcf7a6d11a0ca8fb80890a5f6a8afed40ef4ed
mxchan <mingxiangchan@gmail.com>
2016-08-05 17:14:15 +0800

    listing can be created

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160805085236) do
+ActiveRecord::Schema.define(version: 20160805090355) do
 
   create_table "authentications", force: :cascade do |t|
     t.string   "uid"
@@ -21,6 +21,18 @@ ActiveRecord::Schema.define(version: 20160805085236) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "listings", force: :cascade do |t|
+    t.string   "title"
+    t.integer  "user_id"
+    t.text     "description"
+    t.integer  "max_guests"
+    t.integer  "price"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "listings", ["user_id"], name: "index_listings_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at",                     null: false
